0f6647cd67fd41531ec4633be859c73e807fa89f
ve-ni-di-ci <100785337+ve-ni-di-ci@users.noreply.github.com>
2023-11-29 19:19:04 +0530

    Add files via upload

---
+++ Python Projects/Morse Code Project.py
@@ -0,0 +1,123 @@
+
+
+
+def morse_coder():
+    print('''
+                                                                                                                         
+I8,        8        ,8I            88                                                              888888888888          
+`8b       d8b       d8'            88                                                                   88               
+ "8,     ,8"8,     ,8"             88                                                                   88               
+  Y8     8P Y8     8P   ,adPPYba,  88   ,adPPYba,   ,adPPYba,   88,dPYba,,adPYba,    ,adPPYba,          88   ,adPPYba,   
+  `8b   d8' `8b   d8'  a8P_____88  88  a8"     ""  a8"     "8a  88P'   "88"    "8a  a8P_____88          88  a8"     "8a  
+   `8a a8'   `8a a8'   8PP"""""""  88  8b          8b       d8  88      88      88  8PP"""""""          88  8b       d8  
+    `8a8'     `8a8'    "8b,   ,aa  88  "8a,   ,aa  "8a,   ,a8"  88      88      88  "8b,   ,aa          88  "8a,   ,a8"  
+     `8'       `8'      `"Ybbd8"'  88   `"Ybbd8"'   `"YbbdP"'   88      88      88   `"Ybbd8"'          88   `"YbbdP"'   
+                                                                                                                         
+                                                                                                                         
+''')
+    print(''' 
+ ▄▄       ▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄       ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄   ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
+▐░░▌     ▐░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+▐░▌░▌   ▐░▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀      ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌
+▐░▌▐░▌ ▐░▌▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌               ▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌
+▐░▌ ▐░▐░▌ ▐░▌▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄      ▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌
+▐░▌  ▐░▌  ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
+▐░▌   ▀   ▐░▌▐░▌       ▐░▌▐░█▀▀▀▀█░█▀▀  ▀▀▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀      ▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀█░█▀▀ 
+▐░▌       ▐░▌▐░▌       ▐░▌▐░▌     ▐░▌            ▐░▌▐░▌               ▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌     ▐░▌  
+▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌      ▐░▌  ▄▄▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄      ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌      ▐░▌ 
+▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌▐░▌       ▐░▌
+ ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀   ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀ 
+                                                                                                                                       
+ ''')
+    print("Note: Kindly use alhpanumeric values only.\n")
+    text = [*input('''
+ _______                                 _                                         _                       
+(_______)       _                    _  | |               _                 _     | |                      
+ _____   ____ _| |_ _____  ____    _| |_| |__  _____    _| |_ _____ _   _ _| |_   | |__  _____  ____ _____ 
+|  ___) |  _ (_   _) ___ |/ ___)  (_   _)  _ \| ___ |  (_   _) ___ ( \ / |_   _)  |  _ \| ___ |/ ___) ___ |
+| |_____| | | || |_| ____| |        | |_| | | | ____|    | |_| ____|) X (  | |_   | | | | ____| |   | ____|
+|_______)_| |_| \__)_____)_|         \__)_| |_|_____)     \__)_____|_/ \_)  \__)  |_| |_|_____)_|   |_____)
+                                                                                                           
+\n''').lower()]
+    
+    print(""""  
+          """)
+    morse = []
+    for letter in text:
+        if letter == "a":
+            morse.append(" • — ")
+        elif letter == "b":
+            morse.append(" — ••• ")
+        elif letter == "c":
+            morse.append(" — •— • ")
+        elif letter == "d":
+            morse.append(" — •• ")
+        elif letter == "e":
+            morse.append(" • ")
+        elif letter == "f":
+            morse.append(" ••—• ")
+        elif letter == "g":
+            morse.append(" — —• ")
+        elif letter == "h":
+            morse.append(" •••• ")
+        elif letter == "i":
+            morse.append(" •• ")
+        elif letter == "j":
+            morse.append(" •— — — ")
+        elif letter == "k":
+            morse.append(" —•— ")
+        elif letter == "l":
+            morse.append(" •—•• ")
+        elif letter == "m":
+            morse.append(" — — ")
+        elif letter == "n":
+            morse.append(" —• ")
+        elif letter == "o":
+            morse.append(" — — — ")
+        elif letter == "p":
+            morse.append(" •— —• ")
+        elif letter == "q":
+            morse.append(" — —•— ")
+        elif letter == "r":
+            morse.append(" —•— ")
+        elif letter == "s":
+            morse.append(" ••• ")
+        elif letter == "t":
+            morse.append(" — ")
+        elif letter == "u":
+            morse.append(" ••— ")
+        elif letter == "v":
+            morse.append(" •••— ")
+        elif letter == "w":
+            morse.append(" •— — ")
+        elif letter == "x":
+            morse.append(" —••— ")
+        elif letter == "y":
+            morse.append(" —•— — ")
+        elif letter == "z":
+            morse.append(" — —•• ")
+        elif letter == "1":
+            morse.append(" •— — — — ")
+        elif letter == "2":
+            morse.append(" ••— — — ")
+        elif letter == "3":
+            morse.append(" •••— — ")
+        elif letter == "4":
+            morse.append(" ••••— ")
+        elif letter == "5":
+            morse.append(" ••••• ")
+        elif letter == "6":
+            morse.append(" —•••• ")
+        elif letter == "7":
+            morse.append(" — —••• ")
+        elif letter == "8":
+            morse.append(" — — —•• ")
+        elif letter == "9":
+            morse.append(" — — — —• ")
+        elif letter == "0":
+            morse.append(" — — — — — ")
+        elif letter == " ":
+            morse.append("    ")
+        
+    print(*morse)
+morse_coder()
